e330a16c584455eff82d683cb15b66599f0282ed
ducquang2 <quangbilun@gmail.com>
2021-04-19 19:56:23 +0700

    update

---
+++ src/components/QuoteMachine.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
